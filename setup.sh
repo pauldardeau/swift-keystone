f1d11c720342c604de86d7612ec85af9001a7b7e
Paul Dardeau <pauldardeau@me.com>
2016-06-03 08:47:27 -0500

    add setup-swift-disks.sh and swift-remakerings.sh

--- setup.sh
+++ setup.sh
@@ -5,11 +5,12 @@
 export MY_PUBLIC_IP=`hostname -I | cut -f1 -d' '`
 export MY_IP=`hostname -I | cut -f2 -d' '`
 export MY_PRIVATE_IP=`hostname -I | cut -f2 -d' '`
-
 export OS_RELEASE="mitaka"
 
 ./setup-prereqs.sh
 ./setup-keystone.sh
+./setup-swift-disks.sh
 ./setup-swift.sh
+./swift-remakerings.sh
 ./setup-swift-keystone.sh
 ./start-swift-keystone.sh
