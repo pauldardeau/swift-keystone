5b6ef48d05d68186227d603e6a7ee4f8cb29e24f
Paul Dardeau <pauldardeau@me.com>
2016-06-02 23:10:28 -0500

    consistency with other scripts

--- start-swift-keystone.sh
+++ start-swift-keystone.sh
@@ -2,17 +2,16 @@
 
 # this script starts up swift-keystone
 
+export CREDS_DIR="${HOME}/credentials"
+
 # Start the swift services
 sudo swift-init all restart
 
 # Inspect the running swift services
 sudo swift-init all status
 
-# Navigate back to home directory
-cd
-
 # Use the project 'swiftadmin' credentials
-source ~/credentials/swiftadmin
+source ${CREDS_DIR}/swiftadmin
 
 # Show stats (containers, objects, etc)
 swift stat
